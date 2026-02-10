aa5d95e393b6693ae54bde712d90dd1d3bba2581
shiroiouka <ethereal.crescent@gmail.com>
2026-02-10 23:02:44 +0800

    Update start.py

--- Download/start.py
+++ Download/start.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
         # max_concurrent=5,
         articles_path=r"/Python/mhzx_2D/articles.json",
     )
+    # 下载爬取实例
     spider = MhzxSpider(
         # headless=False,
         articles_path=r"articles.json",
         article_url="https://www.mhh1.com/search/-#eyJrdyI6Iuimi%2BOBmuawtOeFriIsInRhZ3MiOltdLCJjYXQiOltdLCJwYWdlZCI6MSwiY2F0cyI6W119"
     )
 
-    # 运行下载器
+    # 运行
     # downloader.run()
     spider.run()
